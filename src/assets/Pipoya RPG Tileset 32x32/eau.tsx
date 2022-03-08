4de254b7f7eb64cb0ddfce1d720e791eeeb7d030
faune_cld <clement.le-dreau@faune.net>
2022-03-08 10:44:49 +0100

    [+] without stx

--- src/assets/Pipoya RPG Tileset 32x32/eau.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="eau" tilewidth="32" tileheight="32" tilecount="192" columns="24">
- <image source="eau.png" width="790" height="259"/>
-</tileset>
