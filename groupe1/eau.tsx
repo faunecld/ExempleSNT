6c46e39f84920468d04258e9840d25ccce56c67e
faune_cld <clement.le-dreau@faune.net>
2022-03-29 11:38:03 +0200

    before effet numerique

---
+++ groupe1/eau.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="eau" tilewidth="32" tileheight="32" tilecount="49" columns="7">
+ <image source="../groupe6/eau.jpg" width="225" height="225"/>
+</tileset>
---
+++ groupe11/Special_Zones.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Special_Zones" tilewidth="32" tileheight="32" tilecount="12" columns="6">
+ <image source="Special_Zones.png" width="204" height="68"/>
+</tileset>
