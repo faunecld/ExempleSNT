41dcb6c1e387926094d78dc2bb2c4d7f3c80784c
faune_cld <clement.le-dreau@faune.net>
2022-03-29 12:32:46 +0200

    [+] test5 map1

---
+++ src/assets/Pipoya RPG Tileset 32x32/eau.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="eau" tilewidth="32" tileheight="32" tilecount="49" columns="7">
+ <image source="../../../groupe6/eau.jpg" width="225" height="225"/>
+</tileset>
