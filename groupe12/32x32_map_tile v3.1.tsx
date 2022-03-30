b28f53274b14742e5ab0d7988ee5d44f3fd22cd1
faune_cld <clement.le-dreau@faune.net>
2022-03-30 12:53:37 +0200

    bout

---
+++ groupe12/32x32_map_tile v3.1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="32x32_map_tile v3.1" tilewidth="32" tileheight="32" tilecount="667" columns="23">
+ <image source="../groupe1/32x32_map_tile v3.1.png" width="760" height="958"/>
+</tileset>
